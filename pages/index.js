ead0d20a766f655f6367b6c8f46885cf04a38890
MyoThuKha <myothukha@outlook.sg>
2022-10-20 21:59:18 +0630

    added funcs

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useState, useMemo } from "react";
+import { motion } from "framer-motion";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://api.sampleapis.com/coffee/hot");
@@ -9,72 +10,126 @@ export const getStaticProps = async () => {
     props: { data },
   };
 };
+
 export default function Home({ data }) {
-  const displayData = data.slice(0, 4);
+  const [showList, setShowList] = useState(false);
+  const [full, setFull] = useState(false);
+  const [curr, setCurr] = useState(0);
+  const displayData = useMemo(() => {
+    return showList ? data.slice(0, 13) : data.slice(0, 4);
+  }, [showList, data]);
+  const dynamicHeight = showList ? "h-full" : "h-2/5";
   return (
-    <div className=" grid grid-cols-4">
-      <div className="col-span-2 min-h-screen flex items-center">
+    <div className="grid grid-cols-4 h-screen">
+      <div className="col-span-2 flex items-center">
         <div>
           <h1 className="capitalize text-8xl">The taste of coffee</h1>
           <p className="uppercase">viennese & caffee latte</p>
           <p className="uppercase flip-text">Café au lait</p>
         </div>
       </div>
-      <div className="col-span-1 min-h-screen">
-        <div className="relative h-3/5">
-          <Image
-            src="/blue_cup.jpg"
-            alt="blue"
-            layout="fill"
-            objectFit="cover"
-          />
+      {/* col2 */}
+      {full && (
+        <div className="col-span-2 coffeeBrown">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-white"
+              onClick={() => {
+                setFull(false);
+                setShowList(false);
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
         </div>
-        <div className="section3 h-2/5 flex justify-center items-center">
-          <div className="py-2 w-full text-center text-white">
-            {displayData.map((each) => {
-              return (
-                <div key={each.id} className="py-2 cursor-pointer">
-                  <div className="">{each.title}</div>
-                </div>
-              );
-            })}
-            <div className="flex mx-8 justify-end">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                />
-              </svg>
+      )}
+
+      {!full && (
+        <div className="col-span-1">
+          {!showList && (
+            <div className="relative h-3/5">
+              <Image
+                src="/blue_cup.jpg"
+                alt="blue"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          )}
+          <div style={{ backgroundColor: "#402b23" }} className={dynamicHeight}>
+            <div className="">
+              <div className="py-2 text-center text-white">
+                {displayData.map((each) => {
+                  return (
+                    <div key={each.id} className="py-2 cursor-pointer">
+                      <button
+                        onClick={() => {
+                          setFull(true);
+                          setCurr(each.id);
+                        }}
+                      >
+                        {each.title}
+                      </button>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="flex justify-end mr-8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-white"
+                  onClick={() => setShowList(!showList)}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d={
+                      showList
+                        ? "M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
+                        : "M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                    }
+                  />
+                </svg>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="col-span-1">
-        <div className="relative h-2/5">
-          <Image
-            src="/white_cup.jpg"
-            alt="white"
-            layout="fill"
-            objectFit="cover"
-          />
+      )}
+      {/* col3 */}
+      {!full && (
+        <div className="col-span-1">
+          <div className="relative h-2/5">
+            <Image
+              src="/white_cup.jpg"
+              alt="white"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-3/5">
+            <Image
+              src="/yellow_cup.jpg"
+              alt="yellow"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
-        <div className="relative h-3/5">
-          <Image
-            src="/yellow_cup.jpg"
-            alt="yellow"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
